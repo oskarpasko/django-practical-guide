b6026c306e9e9ae36bb4f385d091f4612bf6e139
Oskar <>
2023-03-13 22:33:06 +0100

    add dynamic path

--- mypage/challenges/views.py
+++ mypage/challenges/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseNotFound
+
+challenges = {
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for at least 20 minutes every day!",
+    "march": "Learn Django for at least 20 minutes every day!",
+    "april": "Walk for at least 20 minutes every day!",
+    "may": "Walk for at least 20 minutes every day!",
+    "june": "Walk for at least 20 minutes every day!",
+    "july": "Eat no meat for the entire month!",
+    "august": "Eat no meat for the entire month!",
+    "september": "Walk for at least 20 minutes every day!",
+    "october": "Holidays!",
+    "november": "Walk for at least 20 minutes every day!",
+    "december": "Holidays!"
+}
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("This works!")
+def monthly_challenge_by_number(request, month):
+    challenge_text = None
+    if month == 1:
+        challenge_text = "Eat no meat for the entire month!"
+    elif month == 2:
+        challenge_text = "Walk for at least 20 minutes every day!"
+    elif month == 3:
+        challenge_text = "Learn Django for at least 20 minutes every day!"
+    else:
+        return HttpResponseNotFound("This month is not supported!")
+    return HttpResponse(challenge_text)
+
+def monthly_challenges(request, month):
+    try:
+        challenge_text = challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This month is not supported!")
